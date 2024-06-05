d51159591d6180ca174da7b41aafe55acaf2eeab
josé <jose@pulsarmultimedia.com>
2024-06-05 10:21:51 -0300

    feat: create auth middleware

--- app.js
+++ app.js
@@ -2,16 +2,29 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-  const auth = { login: "yourlogin", password: "yourpassword" };
-  console.log('dasdsadas', req.headers.authorization);
+const basicAuth = (req, res, next) => {
+  // parse login and password from headers
+  const auth = { login: "lalaland", password: "123456" };
+  console.log("aaaa", req.headers.authorization);
   const b64auth = (req.headers.authorization || "").split(" ")[1] || "";
   const [login, password] = Buffer.from(b64auth, "base64")
     .toString()
     .split(":");
 
-  console.log(login, password);
+  //verify login and password are set and correct
+
+  if (login && password && login === auth.login && password === auth.password) {
+    console.log("verificou");
+    return next();
+  } else {
+    // Access denied...
+    res.set("WWW-Authenticate", 'Basic realm="401"'); // change this
+    res.status(401).send("Authentication required."); // custom message
+  }
+};
+
+app.get("/", basicAuth, (req, res) => {
+  res.send("Hello, authenticated user!");
 });
 
 app.listen(port, () => {
